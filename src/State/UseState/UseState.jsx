d1898dfcf34e4be4d8a138c313c9f7f5ac4cf29c
WebNaresh <nareshbhosale73@gmail.com>
2022-10-25 19:14:45 +0530

    10/24/2022

--- src/State/UseState/UseState.jsx
+++ src/State/UseState/UseState.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import UseContext from './UseContext'
-export const UseState = props => {
-  const state = { name: 'harry', class: '5b' }
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import UseContext from "./UseContext";
+export const UseState = (props) => {
+  const state = { name: "harry", class: "5b" };
+
+  const redirect = useNavigate();
   return (
     <UseContext.Provider value={{ state }}>
       {props.children}
     </UseContext.Provider>
-  )
-}
-export default UseState
+  );
+};
+export default UseState;
