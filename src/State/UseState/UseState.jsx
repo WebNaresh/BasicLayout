2b8f8afc020d2e9c81201f0b4b7648d4643eb332
WebNaresh <nareshbhosale73@gmail.com>
2022-10-26 16:52:18 +0530

    26/10/2022

--- src/State/UseState/UseState.jsx
+++ src/State/UseState/UseState.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import UseContext from "./UseContext";
+import { useCookies } from "react-cookie";
 export const UseState = (props) => {
-  const state = { name: "harry", class: "5b" };
+  const [cookies, setCookie, removeCookie] = useCookies(["cookie-name"]);
 
   const redirect = useNavigate();
   return (
-    <UseContext.Provider value={{ state }}>
+    <UseContext.Provider value={{ cookies, setCookie, removeCookie, redirect }}>
       {props.children}
     </UseContext.Provider>
   );
