fcc247a0fafc00ad300959fc367340fada82a055
WebNaresh <nareshbhosale73@gmail.com>
2022-10-30 16:53:48 +0530

    30/10/2022

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
-import TestState from './State/Test/TestState'
-import { BrowserRouter } from 'react-router-dom'
-import Route from './Route'
-import UseEffectState from './State/UseEffect/UseEffectState'
-import UseState from './State/UseState/UseState'
+import TestState from "./State/Test/TestState";
+import { BrowserRouter } from "react-router-dom";
+import Route from "./Route";
+import UseEffectState from "./State/UseEffect/UseEffectState";
+import UseState from "./State/UseState/UseState";
+import AppLoader from "./utils/AppLoader/AppLoader";
+import AppAlert from "./utils/AppAlert/AppAlert";
 
-function App () {
+function App() {
   return (
     <BrowserRouter>
       <UseState>
         <TestState>
           <UseEffectState>
+            <AppLoader />
+            <AppAlert />
             <Route />
           </UseEffectState>
         </TestState>
       </UseState>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
