c310190f4c85fa8a34ff62bba5d065e68222de90
Naresh Bhosale <nareshbhosale73@gmail.com>
2023-02-12 12:55:55 +0530

    after 5 days code completion

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,35 @@ import UseState from "./State/UseState/UseState";
 import AppLoader from "./utils/AppLoader/AppLoader";
 import AppAlert from "./utils/AppAlert/AppAlert";
 import TopLoadingBar from "./utils/TopLoadingBar/TopLoadingBar";
+import TopNav from "./utils/TopNav/TopNav";
+import { colors, createTheme, ThemeProvider } from "@mui/material";
+// mulitple screen size xs,sm,md,lg,xl
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: colors.orange[500],
+    },
+  },
+});
 
 function App() {
   return (
-    <BrowserRouter>
-      <UseState>
-        <TestState>
-          <UseEffectState>
-            <TopLoadingBar />
-            <AppLoader />
-            <AppAlert />
-            <Route />
-          </UseEffectState>
-        </TestState>
-      </UseState>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <UseState>
+          <TestState>
+            <UseEffectState>
+              <TopLoadingBar />
+              <AppLoader />
+              <AppAlert />
+              <TopNav />
+              <Route />
+            </UseEffectState>
+          </TestState>
+        </UseState>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
